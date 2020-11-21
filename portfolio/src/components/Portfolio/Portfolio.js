27e99b5796d4a0c6b7743e48e6e79a745c72d27b
Matheus Bolson <mathbolson@hotmail.com>
2020-11-20 16:21:23 -0800

    added links to portfolio

--- portfolio/src/components/Portfolio/Portfolio.js
+++ portfolio/src/components/Portfolio/Portfolio.js
@@ -7,25 +7,31 @@ function Portfolio() {
   return (
     <div className="Container">
       <div className="caixa"></div>
-      <div class="carousel-wrapper">
+      <div className="carousel-wrapper">
             <Carousel infiniteLoop useKeyboardArrows autoPlay > 
             <div>
                     <img src="../project5.png" alt="" />
+                    <p className="legend"> <a href="https://reastauranttracker.herokuapp.com/"> Restaurant Track </a> </p>
                 </div>
                 <div>
                     <img src="../project4.png" alt="" />
+                    <p className="legend"> <a href="https://burger-eatery.herokuapp.com/"> Eat-da-Burger! </a> </p>
                 </div>
                 <div>
                     <img src="../project1.png" alt="" />
+                    <p className="legend"> <a href="https://mathbolson.github.io/Mood-me/"> Mood-Me </a> </p>
                 </div>
                 <div>
                     <img src="../project2.png" alt=""/>
+                    <p className="legend"> <a href="https://github.com/mathbolson/Note-Taker"> Note Taker </a> </p>
                 </div>
                 <div>
                     <img src="../project3.png" alt="" />
+                    <p className="legend"> <a href="https://github.com/mathbolson/Code-quiz"> Code Quiz </a> </p>
                 </div>
                 <div>
                     <img src="../project6.png" alt="" />
+                    <p className="legend"> <a href="https://trackyourbudgett.herokuapp.com/"> Budget Tracker </a> </p>
                 </div>
             </Carousel>
             <div className="boxs"></div>
